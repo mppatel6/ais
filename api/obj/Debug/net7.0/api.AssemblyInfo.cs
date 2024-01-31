5254bbf75eb9c5ecc0c21966e862ccf726670fc2
Helene <hrenninger@crimson.ua.edu>
2024-01-31 14:10:53 -0600

    hi

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f7ea9e56bb4969930d1ce61a947d1749361bf6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04e1d9f340b2d600f953445e0eb4a9dc76ef5139")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
