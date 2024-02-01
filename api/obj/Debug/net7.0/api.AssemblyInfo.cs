0c31b4ef74b973b243494bee29e1dfc994afba81
Helene <hrenninger@crimson.ua.edu>
2024-02-01 00:49:02 -0600

    moodtracker and water intake structure to dashboard

--- api/obj/Debug/net7.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net7.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b43cbe6f8ea31ad678f7689bb68cafad046bf53")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/resources/scripts/index.js
+++ client/resources/scripts/index.js
@@ -5,13 +5,13 @@ function handleOnLoad(){
     <div class="my-5">
         <div class="p-5 text-center bg-body-tertiary">
         <div class="container py-5">
-            <h1 class="text-body-emphasis">Your Health Hub!</h1>
-            <p class="col-lg-8 mx-auto lead">
-            Some text about the features we offer any why we're unique blah blah blah...
-            </p>
-            <button class ="btn btn-primary" href="signup.html"><a href="signup.html" class="nav-link">Sign-up!</a></button>
-            <button class = "btn btn-secondary" href="signin.html"><a href="signin.html" class="nav-link">Login!</a></button>
-        </div>
+        <h1 class="display-3 fw-bold">Welcome to Beam</h1>
+        <h3 class="fw-normal text-muted mb-3">The all in one mental health app!</h3>
+        <div class="d-flex gap-3 justify-content-center lead fw-normal">
+        <button class ="btn btn-primary" href="signup.html"><a href="signup.html" class="nav-link">Sign-up!</a></button>
+        <button class = "btn btn-secondary" href="signin.html"><a href="signin.html" class="nav-link">Login!</a></button>
+      </div>
+            </div>
         </div>
     </div>`
 
