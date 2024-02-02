cd0eb1c0ec4688e3df5178dcfaaf1e6cbb452d55
Carson Miller <clmiller16@crimson.ua.edu>
2024-02-01 22:54:13 -0600

    buncha code

--- client/resources/scripts/index.js
+++ client/resources/scripts/index.js
@@ -2,8 +2,9 @@ let app = document.getElementById("app")
 
 function handleOnLoad(){
     let html = `
+    <div class="container">
     <div class="my-5">
-        <div class="p-5 text-center bg-body-tertiary">
+        <div class="p-5 text-center bg-body-tertiary rounded">
         <div class="container py-5">
         <h1 class="display-3 fw-bold">Welcome to Beam</h1>
         <h3 class="fw-normal text-muted mb-3">The all in one mental health app!</h3>
@@ -13,6 +14,7 @@ function handleOnLoad(){
       </div>
             </div>
         </div>
+    </div>
     </div>`
 
     document.getElementById('app').innerHTML = html;
