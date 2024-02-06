d4ba7505a3177daadae79f777760dc97e4ecf20c
Manav Patel <manavpatel@Manavs-MacBook-Pro.local>
2024-02-05 21:16:55 -0600

    ugh

--- api/Controllers/JournalController.cs
+++ api/Controllers/JournalController.cs
@@ -19,7 +19,7 @@ namespace api.Controllers
         }
 
         // GET: api/Journal/5
-        [HttpGet("{id}", Name = "Get")]
+        [HttpGet("{id}", Name = "GetJournal")]
         public string Get(int id)
         {
             return "value";
